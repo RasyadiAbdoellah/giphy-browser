62290a3cec54fc7af7a615412f0344919bec1f9e
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-06-27 12:46:08 +0700

    add mount tests for gifList to test child comps - make gifList pass tests

--- src/components/GifList/GifList.test.js
+++ src/components/GifList/GifList.test.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import { shallowLoad } from '../../TestUtils';
+import { shallowLoad, mountLoad } from '../../TestUtils';
 import GifList from './GifList'; //imports from index.js
 
-//shallow tests
 describe('GifList', () => {
-  describe('on a successful req', () => {
-    let component;
-    const dummyProps = { gifs: [{ title: 'bla' }], req: jest.fn };
-    beforeEach(() => {
-      component = shallowLoad(GifList, dummyProps);
-    });
-    it('renders without crashing', () => {});
-    it('renders a list of GifCards', () => {
-      expect(component.exists('GifCard')).toBe(true);
-      expect(component.exists('[data-test="fail-message"]')).toBe(false);
-    });
+  const dummyProps = {
+    gifs: [
+      {
+        id: 'xyz',
+        title: 'bla',
+        images: {
+          fixHeightSmall: {
+            mp4:
+              'https://media1.giphy.com/media/l3fzM2wgd6TygHbYA/100.mp4?cid=fedd91645d143eb3316b526859d10201&rid=100.mp4',
+            url:
+              'https://media1.giphy.com/media/l3fzM2wgd6TygHbYA/100.gif?cid=fedd91645d143eb3316b526859d10201&rid=100.gif'
+          }
+        }
+      }
+    ],
+    select: jest.fn(x => x)
+  };
+  //shallow tests
+  it('renders without crashing', () => {
+    shallowLoad(GifList, dummyProps);
   });
 
-  describe('on a failed req', () => {
+  //mount tests
+  describe('It displays a GifEntry when provided with an array of gifs', () => {
     let component;
-    const dummyProps = { getFail: true };
     beforeEach(() => {
-      component = shallowLoad(GifList, dummyProps);
+      component = mountLoad(GifList, dummyProps);
     });
     it('renders without crashing', () => {});
-    it('renders a list of GifCards', () => {
-      expect(component.exists('GifCard')).toBe(false);
-      expect(component.exists('[data-test="fail-message"]')).toBe(true);
+    it('renders a list of GifEntrys', () => {
+      expect(component.exists('GifEntry')).toBe(true);
+      expect(component.find('GifEntry')).toContainMatchingElements(1, 'GifEntry');
+    });
+    describe('GifEntry shows gifs', () => {
+      it('has source tag with valid url', () => {
+        expect(component.exists('source')).toBe(true);
+        const source = component.find('source');
+        expect(source.prop('src')).toEqual(dummyProps.gifs[0].images.fixHeightSmall.mp4);
+      });
+      it('has gif fallback with valid url', () => {
+        expect(component.exists('img')).toBe(true);
+        const img = component.find('img');
+        expect(img.prop('src')).toEqual(dummyProps.gifs[0].images.fixHeightSmall.url);
+      });
     });
   });
 });
-
-//mount tests (for gifList behaviour when given data)
